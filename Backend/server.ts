b52dad7b4443ed22e6a113ab19e328cbcaaa4904
siddiq55 <msiddiq5565@gmail.com>
2026-03-08 12:40:05 +0500

    cookies properties updated

--- Backend/server.ts
+++ Backend/server.ts
@@ -35,7 +35,12 @@ app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 3}, // 3 day
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 3,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      path: '/'
+    }, // 3 day
     store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI as string,
         collectionName: "sessions",
